be00a11e6150d8e3084216570ee256e6c4d785c7
T-SarvesH <sarvesh1.tikekar@gmail.com>
2025-09-15 14:32:14 +0530

    Some minor updates

---
+++ backend/Object_Detection_and_Scene_Description_Model/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ObjectDetectionAndSceneDescriptionModelConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Object_Detection_and_Scene_Description_Model'
--- backend/core/settings.py
+++ backend/core/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
+    'Object_Detection_and_Scene_Description_Model',
 ]
 
 MIDDLEWARE = [
