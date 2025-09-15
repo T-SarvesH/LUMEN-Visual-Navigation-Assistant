5cc17ddea0120fcc560b1cc32b615ab3ef83d2ed
T-SarvesH <sarvesh1.tikekar@gmail.com>
2025-09-15 07:21:33 +0530

    Working on IMU + Localization now so yeah the project is officially officially on

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,6 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.middleware import CORSMiddleware
+import json
+import logging
 
+#Importing all API models
+from models import coordinates
 
 app = FastAPI(title="Lumen Application Programming interface")
 
@@ -13,7 +17,34 @@ app.add_middleware(
     allowed_methods=["*"],
 )
 
-
 @app.get("/")
 async def root():
     return {"message": "Welcome to Lumen API"}
+
+
+# Websocket endpoint for IMU data
+@app.websocket("/ws/vioService")
+async def vioService(websocket: WebSocket):
+
+    await websocket.accept()
+    try:
+        while True:
+            data_str = await websocket.receive()
+            data_pack = json.loads(data_str)
+
+            if 'imu_reading' in data_pack:
+
+                imu_data = data_pack['imu_reading']
+
+                response = coordinates(
+
+                    coord_x=imu_data['x'],
+                    coord_y=imu_data['y'],
+                    coord_z=imu_data['z']
+                )
+                await websocket.send_text(json.dumps({'coordinates': response.model_dump()}))
+
+    except WebSocketDisconnect:
+        return {'message': 'Client is Disconnected'}
+    except Exception as e:
+        return {'Exception': str(e)}
--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from pydantic import BaseModel, Field
+from typing import DefaultDict, List, Dict
 
-# Create your models here.
+# Model 1: For Coordinate data
+class coordinates(BaseModel): 
+
+    coord_x: int = Field(..., examples=1)
+    coord_y: int = Field(..., examples=1)
+    coord_z: int = Field(..., examples=1)
+
+    
